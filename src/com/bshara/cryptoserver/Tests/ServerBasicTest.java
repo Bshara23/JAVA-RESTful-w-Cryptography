300ee4db7389853ccded98402ea2ab41ec8a3f38
Bshara Zahran <bshara23@gmail.com>
2020-09-17 10:28:16 +0300

    added dep

---
+++ src/com/bshara/cryptoserver/Tests/ServerBasicTest.java
@@ -0,0 +1,14 @@
+package com.bshara.cryptoserver.Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ServerBasicTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
